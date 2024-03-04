7dcbe4985a15c8b532c821c363bfccda9d8fac8d
twilio08 <ravishankar86022@gmail.com>
2024-03-04 08:50:37 +0530

    counter app completed using UseState hook

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -8,12 +8,35 @@ function App() {
 
 
 
+    const addValue = () => {
+
+        if(counter<20){
+
+            setCounter(counter+1);
+        }
+    }
+
+    const removeValue = () => {
+
+        if(counter>0){
+
+            setCounter(counter-1);
+        }
+    }
+
 
   return(
 
 <>
 
+<h1>Chai Aur Code</h1>
+<h2>Counter</h2>
 
+<button onClick={addValue}>AddValue</button>
+<br />
+<h1>{counter}</h1>
+<br />
+<button onClick={removeValue}>RemoveValue</button>
 </>
 
 
