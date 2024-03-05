d0a0cd7a7ced78272002bd82d21ec70747507560
twilio08 <ravishankar86022@gmail.com>
2024-03-05 18:29:23 +0530

    added logic for getting random password and used useCallback hook

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,15 +1,41 @@
-import {useState} from 'react';
-import './App.css'
+import {useCallback, useState} from 'react';
 
 function App() {
 
-
   const [length , setLength] = useState(8);
+  const [numberAllowed , setNumberAllowed] = useState(false);
+  const [charAllowed , setCharAllowed] = useState(false);
+  const [password , setPassword] = useState("");
+
+
+  const passwordGenerator = useCallback(()=>{
+
+    let pass = "";
+    let str = "ABCDEFGHIJLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+
+    if(numberAllowed)  str += "0123456789";
+    if(charAllowed)   str += "~!@#$%^&*()[]{}";
+
+
+    for(let index = 1 ; index<Array.length; index++) {
+
+      let char =Math.floor(  Math.random() * str.length +1 );
+
+      pass = str.charAt(char);
+
+    }
+
+    setPassword(pass);
+
+  } , [length, numberAllowed , charAllowed , setPassword]);
+
+  
   return (
     <>
       
+      
 
-      <h1 className="text-4xl text-center">Password Generator</h1>
 
 
     </>
