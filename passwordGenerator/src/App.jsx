03512c53830b993cfb9ee1e2d2b08331ae1b6c70
twilio08 <ravishankar86022@gmail.com>
2024-03-05 19:30:25 +0530

    ui devlopment completed

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     if(charAllowed)   str += "~!@#$%^&*()[]{}";
 
 
-    for(let index = 1 ; index<Array.length; index++) {
+    for(let index = 1 ; index<length; index++) {
 
       let char =Math.floor(  Math.random() * str.length +1 );
 
@@ -34,6 +34,85 @@ function App() {
   return (
     <>
       
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        
+
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+
+           
+           <input type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='password'
+            readOnly
+           />
+
+           <button className="outline-none bg-blue-500 text-white px-3 py-5 shrink-0">Copy</button>
+   
+
+        </div>
+ 
+        <div className='flex text-sm gap-x-2'>
+
+           <div className='flex items-center gap-x-1'>
+
+             <input 
+             type="range"
+             min={8}
+             max={100}
+             value={length}
+             className='cursor-pointer'
+             onChange={(e)=> {setLength(e.target.value)}}
+             />
+
+             <label>Length:{length}</label>
+
+
+           </div>
+
+           <div className="flex items-center gap-x-1">
+
+           <input
+            type="checkbox" 
+            defaultChecked = {numberAllowed}
+            id="number Input"
+            onChange={()=>{
+
+              setNumberAllowed((prev)=> !prev);
+            }} 
+            
+            />
+
+            <label htmlFor="numberInput">Numbers</label>
+
+           </div>
+
+
+           <div className="flex items-center gap-x-1">
+
+           <input
+            type="checkbox" 
+            defaultChecked = {charAllowed}
+            id="characterInput"
+            onChange={()=>{
+
+              setCharAllowed((prev)=> !prev);
+            }} 
+            
+            />
+
+            <label htmlFor="characterInput">Characters</label>
+
+           </div>
+
+        </div>
+
+        
+
+
+      </div>
       
 
 
