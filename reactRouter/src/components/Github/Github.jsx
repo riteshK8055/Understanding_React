a4631b0ffed8e7301166635f628b009a24cfedba
riteshK8055 <riteshk8055@gmail.com>
2024-03-09 12:57:27 +0530

    added component of git link

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom';
 
 function Github() {
 
-    const [data , setData] = useState([]);
-    useEffect(()=>{
+  const data =  useLoaderData()
+    // const [data , setData] = useState([]);
+    // useEffect(()=>{
 
-        fetch('https://api.github.com/users/riteshk8055')
-        .then(response => response.json())
-        .then(data => {
+    //     fetch('https://api.github.com/users/riteshk8055')
+    //     .then(response => response.json())
+    //     .then(data => {
 
-            setData(data)
-        })
-    }, [])
+    //         setData(data)
+    //     })
+    // }, [])
   return (
-    <div className='text-center m-4 bg-gray-600 text-white text-3xl'>Github followers :- {data.followers}</div>
+    <div className='text-center m-4 bg-gray-600 text-white text-3xl'>Github followers :- {data.followers}
+    <img src={data.avatar_url} alt="Git Picture" width={300}/>
+    </div>
+    
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async() =>{
+
+  const response = await fetch('https://api.github.com/users/riteshk8055');
+  return response.json()
+}
