5a68e14c27625226178d5409b1febda9e03e1d2d
riteshK8055 <riteshk8055@gmail.com>
2024-03-11 16:36:50 +0530

    basic functionality added

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,13 +1,46 @@
 import { useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [themeMode , setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+
+    setThemeMode("light");
+  }
+
+  const darkTheme = () => {
+
+    setThemeMode("dark");
+  }
+
+  // Actual change in theme 
+
+  useEffect(() => {
+
+    document.querySelector('html').classList.remove("light","dark");
+    document.querySelector('html').classList.add(themeMode);
+  }, [themeMode])
 
   return (
-    <>
-      <h1></h1>
-    </>
+    
+    <ThemeProvider value= {{themeMode , lightTheme , darkTheme}}>
+
+       <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* THEME BUTTON */}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                        {/* CARD */}
+                    </div>
+             </div>
+       </div>
+    
+    </ThemeProvider>
   )
 }
 
