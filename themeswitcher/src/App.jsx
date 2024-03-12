1c1988f5d9a4e1e51f304bb538b7ab975d8a543c
riteshK8055 <riteshk8055@gmail.com>
2024-03-12 19:56:32 +0530

    added functionality in App.js and completed this project

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import './App.css'
-import { ThemeProvider } from './contexts/theme'
+import { ThemeProvider } from './contexts/theme.js'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
   
@@ -31,11 +33,13 @@ function App() {
        <div className="flex flex-wrap min-h-screen items-center">
             <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* THEME BUTTON */}
+                        <ThemeBtn />
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                        {/* CARD */}
+                        
+                        <Card />
+
                     </div>
              </div>
        </div>
