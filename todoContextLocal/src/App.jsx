44008adb47bb17b234343778d901d93edbb79b99
riteshK8055 <riteshk8055@gmail.com>
2024-03-15 21:57:31 +0530

    completed this project with all errors resolved

--- todoContextLocal/src/App.jsx
+++ todoContextLocal/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { TodoProvider } from "./contexts"
+import { TodoForm, TodoItem } from "./components"
 
 
 function App() {
@@ -35,7 +36,7 @@ function App() {
 
   const toggleComplete = (id) => {
 
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed}: prevTodo))
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id=== id ? {...prevTodo, completed: !prevTodo.completed}: prevTodo))
   }
 
 
@@ -67,9 +68,23 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+
+                           <div key={todo.id}
+                           className="w-full"
+                           >
+
+                            <TodoItem todo = {todo}/>
+
+                              
+
+                           </div>
+
+                        ))}
                     </div>
                 </div>
             </div>
