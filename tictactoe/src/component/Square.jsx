88b4335ce4cbaef2c5fb58365f4f985473bbd893
riteshK8055 <riteshk8055@gmail.com>
2024-03-20 22:42:23 +0530

    added Square Component

---
+++ tictactoe/src/component/Square.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Square = () => {
+
+    return (
+
+        <>
+        
+          <div
+          style={{border : '1px solid',
+                  height: '100px',
+                  width: "100%", 
+                  display: 'flex' , 
+                  justifyContent: "center", 
+                  alignItems:"center"
+                }} 
+          className="square">
+            <h2>X</h2>
+          </div>
+        </>
+    );
+};
+
+export default Square;
