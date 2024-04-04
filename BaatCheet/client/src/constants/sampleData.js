2164822ca6da1b1fae4c4e0de81d9c54ddec54dc
riteshK8055 <riteshk8055@gmail.com>
2024-04-04 23:25:41 +0530

    chat app project using react 20% done

---
+++ BaatCheet/client/src/constants/sampleData.js
@@ -0,0 +1,24 @@
+
+
+export const sampleChats = [
+  
+  {
+
+    avatar:[],
+    name: "John Doe",
+    _id: "1",
+    groupChat : false,
+    members:["1" , "2"],
+  },
+
+  
+  {
+
+    avatar:[],
+    name: "John Boi",
+    _id: "2",
+    groupChat : false,
+    members:["1" , "2"],
+  }
+ 
+];
---
+++ BaatCheet/client/src/pages/Groups.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Groups = () => {
+
+    return (
+
+        <div>Group</div>
+    );
+};
+
+export default Groups;
