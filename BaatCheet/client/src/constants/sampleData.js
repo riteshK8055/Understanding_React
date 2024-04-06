27480735abf5f45386a9f7524c1cc46de3b56cfc
riteshK8055 <riteshk8055@gmail.com>
2024-04-06 15:48:34 +0530

    added search dialoge

--- BaatCheet/client/src/constants/sampleData.js
+++ BaatCheet/client/src/constants/sampleData.js
@@ -22,3 +22,20 @@ export const sampleChats = [
   }
  
 ];
+
+export const sampleUsers = [
+
+  {
+
+    avatar:["https://www.w3schools.com/howto/img_avatar.png" ,  "https://www.w3schools.com/howto/img_avatar.png" ],
+    name: "John Doe",
+    _id: "1",
+  },
+
+  {
+
+    avatar:["https://www.w3schools.com/howto/img_avatar.png" ,  "https://www.w3schools.com/howto/img_avatar.png" ],
+    name: "John Boi",
+    _id: "2",
+  },
+]
