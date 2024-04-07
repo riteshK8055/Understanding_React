8d526cb4f0bcc29badfff79670cc3501966ef841
riteshK8055 <riteshk8055@gmail.com>
2024-04-07 08:41:37 +0530

    home component complete

--- BaatCheet/client/src/pages/Home.jsx
+++ BaatCheet/client/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import {Typography, Box} from "@mui/material";
 import AppLayout from "../components/layout/AppLayout";
 
 const Home = () => {
 
     return (
 
-        <div>Home</div>
+       <Box bgcolor={"lightgray"} height={"100%"}> 
+        <Typography p={"2rem"} variant="h5" textAlign={"center"}>
+            Select a friend to chat.
+        </Typography>
+      </Box>
     )
 };
 
