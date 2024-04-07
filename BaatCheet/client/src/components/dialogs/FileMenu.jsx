8c1e71e084a16d655730ab776cb861b5be6b689d
riteshK8055 <riteshk8055@gmail.com>
2024-04-07 10:45:27 +0530

    added file menu component functionality will be added later

---
+++ BaatCheet/client/src/components/dialogs/FileMenu.jsx
@@ -0,0 +1,10 @@
+import { Menu } from '@mui/icons-material'
+import React from 'react'
+
+const FileMenu = ({anchorE1}) => {
+  return (
+    <Menu anchorE1 = {anchorE1}></Menu>
+  )
+}
+
+export default FileMenu
