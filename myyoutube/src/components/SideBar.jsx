1f2973b644cc7f884ccd1c6893fae18d221e4002
riteshK8055 <riteshk8055@gmail.com>
2024-04-13 23:07:52 +0530

    sidebar complete

--- myyoutube/src/components/SideBar.jsx
+++ myyoutube/src/components/SideBar.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import MenuItems from './MenuItems'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faHouse , faFilm, faStar , faUserPlus} from '@fortawesome/free-solid-svg-icons';
+
 
 const SideBar = () => {
   return (
-    <div>
-        <h1 className="font-bold">Sidebar</h1>
-        <MenuItems />
-    </div>
+    <ul className='w-24 m-2 flex flex-col'>
+  <li className="hover:bg-gray-200 rounded-lg flex flex-col items-center py-5">
+    <button className='p-1'><FontAwesomeIcon className="h-6" icon={faHouse} /></button>
+    <span>Home</span>
+  </li>
+  <li className="hover:bg-gray-200 rounded-lg flex flex-col items-center py-5">
+    <button className="p-1"><FontAwesomeIcon className="h-6" icon={faFilm} /></button>
+    <span>Shorts</span>
+  </li>
+  <li className="hover:bg-gray-200 rounded-lg flex flex-col items-center py-5">
+    <button className="p-1"><FontAwesomeIcon className="h-6" icon={faStar} /></button>
+    <span>Subscriptions</span>
+  </li>
+  <li className="hover:bg-gray-200 rounded-lg flex flex-col items-center py-5">
+    <button className="p-1"><FontAwesomeIcon className="h-6" icon={faUserPlus} /></button>
+    <span>You</span>
+  </li>
+</ul>
   )
 }
 
